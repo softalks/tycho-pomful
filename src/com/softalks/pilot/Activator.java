7c93525332626145cd82a782e0bf0a3b4eddfebd
Santiago Ruiz Hiebra <hiebra@hotmail.com>
2016-04-15 23:03:17 +0200

    Update Activator.java

--- src/com/softalks/pilot/Activator.java
+++ src/com/softalks/pilot/Activator.java
@@ -6,7 +6,7 @@ import org.osgi.framework.BundleContext;
 public class Activator implements BundleActivator {
 
 	public void start(BundleContext bundleContext) throws Exception {
-		System.out.println("Hello, World!");
+		System.out.println("Hello, CI World!");
 	}
 
 	public void stop(BundleContext bundleContext) throws Exception {
